4dd30aeabc6d314cd24c9189559c94f535a53136
tatsenko-m <tatsenko.m@yandex.ru>
2023-07-05 16:03:16 +0300

    feat: реализовать логирование запросов и ошибок

--- backend/app.js
+++ backend/app.js
@@ -10,12 +10,14 @@ const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/error-handler');
 const router = require('./routes/index');
 const { validateSignup, validateSignin } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(requestLogger);
 
 mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
   useNewUrlParser: true,
@@ -30,6 +32,8 @@ app.use(auth);
 app.use(router);
 
 app.listen(3000);
+
+app.use(errorLogger);
 
 app.use(errors());
 
